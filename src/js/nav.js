d2367bac4928cf91174fe36c6237498c7ace4779
”Enne” <EnneFofe10@gmail.com>
2024-07-27 17:56:38 -0300

    Serviços - Página Secundária
    Codando a página de serviços com seus devidos tamanhos adequados para dispositivos desktop e mobile, além de fazer alguns ajustes em alguns detalhes

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,7 +1,7 @@
 const ops = document.querySelectorAll(".nav ul a")
 ops.forEach((op) => {
     op.addEventListener("click", () => {
-        op.style.backgroundColor = "#E057F2"
+        op.style.background = "radial-gradient(78.22% 70.71% at 50% 50%, #E057F2 50%, #C522F2 100%)"
         op.style.color = "white"
     })
 })
