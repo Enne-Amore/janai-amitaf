1bc07e125d9ef9b144072aeb4ce9dec9f4395944
Enne Jim Pessoa <ennefofe10@gmail.com>
2025-07-11 21:19:22 -0300

    feat: update meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,20 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Janai Amitáf Produções",
-  description: "Janai Amitáf Produções apresenta seu portfólio web com seus projetos, serviços, produtos, e demais obras artísticas",
+  description: "Janai Amitáf Produções apresenta seu portfólio web com seus projetos, serviços, produtos, e demais trabalhos artísticos",
+  openGraph: {
+    title: "Janai Amitáf Produções - Portfólio Web",
+    description: "Janai Amitáf Produções apresenta seu portfólio web com seus projetos, serviços, produtos, e demais trabalhos artísticos",
+    images: ['../assets/logo.png']
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    }
+  }
 };
 
 export default function RootLayout({
