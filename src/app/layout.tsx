505be711f1c0138bba0813ad7d4a87ba8e0062fb
Enne Jim Pessoa <ennefofe10@gmail.com>
2025-08-26 12:24:34 -0300

    feat: add more meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,28 @@ export const metadata: Metadata = {
   title: "Janai Amitáf Produções",
   description:
     "Janai Amitáf Produções apresenta seu portfólio web com seus projetos, serviços, produtos, e demais trabalhos artísticos",
+  authors: [{ name: "Janai Amitáf Produções", url: "https://janai-amitaf.vercel.app" }],
   openGraph: {
+    type: "website",
     title: "Janai Amitáf Produções",
     description:
       "Janai Amitáf Produções apresenta seu portfólio web com seus projetos, serviços, produtos, e demais trabalhos artísticos",
     images: ["https://github.com/Enne-Amore/janai-amitaf/blob/main/docs/favicon/logo.png?raw=true"],
+    url: "https://janai-amitaf.vercel.app",
+    siteName: "Janai Amitáf Produções"
   },
+  twitter: {
+    card: "summary_large_image",
+    site: "https://janai-amitaf.vercel.app",
+    title: "Janai Amitáf Produções",
+    description: "Janai Amitáf Produções apresenta seu portfólio web com seus projetos, serviços, produtos, e demais trabalhos artísticos",
+    images: "https://github.com/Enne-Amore/janai-amitaf/blob/main/public/favicon/logo.png?raw=true"
+  },
+  icons: {
+    icon: "/public/favicon/favicon-32x32.png",
+    apple: "/public/favicon/apple-touch-icon.png",
+  },
+  manifest: "/public/favicon/site.webmanifest",
   verification: {
     google: "N2dwzMN_2Th96csMroFlIe8sR3g7oWyBad-oQcxxWxA"
   },
