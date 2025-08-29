4450a0055aa5b15565c2267076734f6cf772934e
Enne Jim Pessoa <ennefofe10@gmail.com>
2025-08-29 14:21:34 -0300

    fix: accessibility by tab key

--- src/components/linksListsMenu/index.tsx
+++ src/components/linksListsMenu/index.tsx
@@ -12,7 +12,7 @@ export default function LinksListsMenu(props: LinksListsMenuInterface) {
         target="_blank"
         rel="noopener noreferrer"
         aria-label={props.ariaLabel}
-        className={`text-[#C522F2] text-lg lg:text-xl text-shadow-2xs px-2 lg:px-4 py-1 lg:py-2 rounded-4xl transition hover:bg-[#C522F2] hover:text-[#FCFAFF] active:bg-[#6C0CF2]`}
+        className={`text-[#C522F2] text-lg lg:text-xl text-shadow-2xs px-2 lg:px-4 py-1 lg:py-2 rounded-4xl transition hover:bg-[#C522F2] focus-visible:bg-[#C522F2] hover:text-[#FCFAFF] focus-visible:text-[#FCFAFF] active:bg-[#6C0CF2]`}
       >
         {props.label}
       </a>
