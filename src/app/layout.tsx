662ea829b8fe6b010628d70891e9ffd867fb9956
Enne Jim Pessoa <ennefofe10@gmail.com>
2025-09-02 11:25:31 -0300

    fix: image og url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
     title: "Janai Amitáf Produções",
     description:
       "Janai Amitáf Produções apresenta seu portfólio web com seus projetos, serviços, produtos, e demais trabalhos artísticos",
-    images: ["https://github.com/Enne-Amore/janai-amitaf/blob/main/docs/favicon/logo.png?raw=true"],
+    images: "https://github.com/Enne-Amore/janai-amitaf/blob/main/public/favicon/logo.png?raw=true",
     url: "https://janai-amitaf.vercel.app",
     siteName: "Janai Amitáf Produções"
   },
